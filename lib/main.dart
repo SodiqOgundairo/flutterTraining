3d1d1b7ec75c491ad24d8a4f3909e47e0bcb1777
Sodiqogundairo <kingmclamba1@gmail.com>
2023-08-07 13:56:59 +0100

    onboard page done

--- lib/main.dart
+++ lib/main.dart
@@ -31,12 +31,44 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Map Blog'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text('Welcome!'),
+      backgroundColor: Colors.white,
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 77, 152, 213),
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              'Mapp Blog',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 77, 152, 213),
+                  fontSize: 25,
+                  fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(height: 15.0),
+            Image.asset('assets/images/welcome.png'),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(300, 40),
+              ),
+              child: const Text('Login'),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                  minimumSize: const Size(300, 40),
+                  foregroundColor: Colors.blue),
+              child: const Text('SignUp'),
+            )
+          ],
+        ),
       ),
     );
   }
